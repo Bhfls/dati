aca46de72155b09a681879b72206821aa7232fc1
Fu-Yuhan <19906613880@163.com>
2024-11-16 04:18:45 +0000

    until 40

---
+++ 35/main.py
@@ -0,0 +1,7 @@
+# BEGIN: 9yc0r9r94x4x
+import math
+
+radius = float(input())
+volume = (4 / 3) * math.pi * (radius ** 3)
+print(f"{volume:.2f}")
+# END: 9yc0r9r94x4x
---
+++ 39/mian.py
@@ -0,0 +1,10 @@
+# BEGIN: 9ycj9s34x434
+dividend = int(input())
+divisor = int(input())
+
+quotient = dividend // divisor
+remainder = dividend % divisor
+
+print("商为：", quotient)
+print("余数为：", remainder)
+# END: 9ycj9s34x434
