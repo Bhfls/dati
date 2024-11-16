becc9396a5000df5c1dabe9c943bf951b16c3517
Fu-Yuhan <19906613880@163.com>
2024-11-16 04:26:19 +0000

    until 50

---
+++ 42/mian.py
@@ -0,0 +1,5 @@
+# BEGIN: 9ldx9r9j4f4
+ascii_code = int(input())
+character = chr(ascii_code)
+print(character)
+# END: 9ldx9r9j4f4
---
+++ 46/mian.py
@@ -0,0 +1,9 @@
+# BEGIN: 9ycj0n943x4
+c1, c2, c3, c4, c5 = 'C', 'h', 'i', 'n', 'a'
+c1 = chr(ord(c1) + 4)
+c2 = chr(ord(c2) + 4)
+c3 = chr(ord(c3) + 4)
+c4 = chr(ord(c4) + 4)
+c5 = chr(ord(c5) + 4)
+print(c1, c2, c3, c4, c5, sep='')
+# END: 9ycj0n943x4
