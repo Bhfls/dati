2beb774c238ad937a8b1bc1a18017ffed8f87fe9
Fu Yuhan <152140410+Fu-Yuhan@users.noreply.github.com>
2024-11-25 13:54:50 +0800

    添加题解 (#7)

---
+++ 141/main.py
@@ -0,0 +1,5 @@
+day = int(input())
+if day not in [1, 3, 5]:
+    print("YES")
+else:
+    print("NO")
---
+++ 150/main.py
@@ -0,0 +1,7 @@
+# BEGIN: 9ycj4x444x4r
+for i in range(26):
+    print(chr(97 + i), end=' ')
+print()
+for i in range(25, -1, -1):
+    print(chr(65 + i), end=' ')
+# END: 9ycj4x444x4r
