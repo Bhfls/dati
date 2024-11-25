1236a7b74302468bf2b9d39109270f874ab0799b
Fu-Yuhan <19906613880@163.com>
2024-11-25 05:53:47 +0000

    添加题解

---
+++ 142/main.py
@@ -0,0 +1,10 @@
+char = input("请输入一个字符：")
+
+if char == 'a':
+    print("apple")
+elif char == 'b':
+    print("banana")
+elif char == 'c':
+    print("cat")
+else:
+    print("no")
---
+++ 146/main.py
@@ -0,0 +1,3 @@
+for i in range(1, 13):
+    print(f"老狼老狼几点钟？{i}点钟。")
+print("狼来了，快跑！")
