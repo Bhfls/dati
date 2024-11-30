574f3ed6524c12dc1091005a702742f8fbe8c6a7
Fu-Yuhan <19906613880@163.com>
2024-11-30 01:46:23 +0000

    finish 100

---
+++ 100/main.py
@@ -0,0 +1,7 @@
+# BEGIN: 9ycx8p443j9r
+a, b = map(int, input().split())
+if b % a == 0:
+    print(b // a)
+else:
+    print(b // a + 1)
+# END: 9ycx8p443j9r
---
+++ 98/main.py
@@ -0,0 +1,8 @@
+# BEGIN: 90x5j9kckfd
+x, y = map(int, input().split())
+total_sleep_time = x/3 + y
+if total_sleep_time >= 8:
+    print("Yes")
+else:
+    print("No")
+# END: 90x5j9kckfd
