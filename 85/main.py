b1d2f944714bbeb2399e1281a0c0283d37d60832
Fu-Yuhan <19906613880@163.com>
2024-12-04 13:30:40 +0000

    fix 85 #32

--- 85/main.py
+++ 85/main.py
@@ -1,2 +1,7 @@
 num1, num2, num3 = map(float, input().split())
-print(max(num1, num2, num3))
+max_num = max(num1, num2, num3)
+if max_num.is_integer():
+    max_num_str = str(max_num).rstrip('0').rstrip('.')
+    print(max_num_str)
+else:
+    print(max_num)
